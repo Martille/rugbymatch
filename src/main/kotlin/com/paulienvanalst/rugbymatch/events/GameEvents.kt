156db8e8e0e8534d0caa9ace50c806e4afa1a264
Charlotte PLANCQ <charlotte.plancq@gmail.com>
2019-04-18 13:19:30 +0200

    ex2 solution

--- src/main/kotlin/com/paulienvanalst/rugbymatch/events/GameEvents.kt
+++ src/main/kotlin/com/paulienvanalst/rugbymatch/events/GameEvents.kt
@@ -1,9 +1,22 @@
 package com.paulienvanalst.rugbymatch.events
 
+import com.paulienvanalst.rugbymatch.game.LineOut
+import com.paulienvanalst.rugbymatch.game.Scrum
 import com.paulienvanalst.rugbymatch.game.SetPiece
 import com.paulienvanalst.rugbymatch.team.TeamName
 import org.springframework.context.ApplicationEvent
 
 sealed class SetPieceEvent(source: Any, val setPiece: SetPiece, val winningTeam: TeamName) : ApplicationEvent(source)
+class ScrumWasPlayed(source: Any, scrum: Scrum, winningTeam: TeamName) : SetPieceEvent(source, scrum, winningTeam)
+class LineOutWasPlayed(source: Any, lineOut: LineOut, winningTeam: TeamName) : SetPieceEvent(source, lineOut, winningTeam)
 
+fun List<SetPieceEvent>.wonBy(teamName: TeamName) : List<SetPiece> {
+    return this.filter { it.winningTeam == teamName }.map { it.setPiece }
+}
 
+fun List<SetPieceEvent>.lostBy(teamName: TeamName) : List<SetPiece> {
+    return this.filter { it.winningTeam != teamName }.map { it.setPiece }
+}
+
+fun List<SetPieceEvent>.scrumEvents() : List<SetPieceEvent> = this.filter { it is ScrumWasPlayed }
+fun List<SetPieceEvent>.lineOutEvents() : List<SetPieceEvent> = this.filter { it is LineOutWasPlayed }
